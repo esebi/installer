9b3c16118d37b94c41c2483f909392201d41c4da
The Oksijen <68513016+david-mammedov@users.noreply.github.com>
2021-05-29 23:19:26 +0400

    first commit

---
+++ ios.sh
@@ -0,0 +1,27 @@
+MESAJ="NeonUserBot UserBot Ucun ios kurulumu"
+MESAJ+="\nTelegram: @NeonUserBot"
+echo y | apk update
+clear
+echo -e $MESAJ
+echo "Python Yüklənir"
+echo y | apk add python3
+clear
+echo -e $MESAJ
+echo "Git Yüklənir"
+echo y | apk add git
+clear
+echo -e $MESAJ
+echo "TeleThon Yüklənir"
+python3 -m pip install telethon
+echo "Repo Yüklənir..."
+git clone https://github.com/erdembey1/siriinstaller
+clear
+echo -e $MESAJ
+clear
+echo -e $MESAJ
+echo " requirements Yüklənir"
+cd neon_installer
+python3 -m pip install wheel
+python3 -m pip install -r requirements.txt
+clear
+python3 -m neon_installer
