a8cfd3ba6e822186df7134256c3fd0215a3d8214
The Oksijen <68513016+TheOksigen@users.noreply.github.com>
2021-06-11 13:39:55 +0400

    Update android.sh
    Dd

--- android.sh
+++ android.sh
@@ -1,67 +1,27 @@
 
-N+="\n| \ | |__   / _ \| \ | |"
-N+="\n|  \| |  _|| | | |  \| |"
-N+="\n| |\  | |__| |_| | |\  |"
-N+="\n| |\  | |__| |_| | |\  |"
-N+="\n|_| \_|_____\___/|_| \_|"
-N+="\n✨NEON USERBOT INSTALLER✨"
-MESAJ="\n📱NEON UserBot Ucun android kurulumu📱"
-MESAJ+="\n "
-MESAJ+="\n📣 Rəsmi Kanal; @NeonUserbot"
-MESAJ+="\n🆘 Syppart Qrupu; @NeonSupport"
-MESAJ+="\n🧩 Plugin Kanalı; @NeonPlugin"
-MESAJ+="\n "
-MESAJ+="\n❗Yüklənmə bitməyənə qədər bu ekranı tərk etməyin❗"
-HELP="\n❗❗ %50, %70 və %75'də "Y" Yazıb enter basın  ❗❗"
-HELP+="\n "
-BOSLUK="\n "
-echo -e $N
-echo -e $HELP
-echo "⏳ TERMUX UPDATE ⏳"
-echo "⏳ I UPDATE YOUR REQUIREMENTS ⏳"
-echo -e $BOSLUK
+MESAJ="NeonUserBot Avtomatik Qurulumuna Xoş Gəldiniz!"
+MESAJ+="\nTG: @NeonUserBot"
 pkg update -y
 clear
-echo -e $N
-echo -e $BOSLUK
 echo -e $MESAJ
-echo "⌛ SMARTFONUNUZSA PYTHON YÜKLƏNİR ⌛"
-echo "⌛ PYTHON IS INSTALLED ON YOUR DEVICE ⌛"
-echo -e $BOSLUK
+echo "Python Yüklənir"
 pkg install python -y
-pip install --upgrade pip
 clear
-echo -e $ N
 echo -e $MESAJ
-echo -e $BOSLUK
-echo "⌛ GIT QURULUR ⌛"
-echo "⌛ INSTALLING GIT ⌛"
-echo -e $BOSLUK
+echo "Git Yüklənir"
 pkg install git -y
 clear
-echo -e $N
 echo -e $MESAJ
-echo -e $BOSLUK
-echo "⌛ TELETHON QURULUR ⌛"
-echo "⌛ INSTALLING TELETHON ⌛"
-echo -e $BOSLUK
+echo "TeleThon Yüklənir"
 pip install telethon
+echo "Repo klonlanır..."
+git clone https://github.com/TheOksigen/Neon_installer
 clear
-echo -e $N
 echo -e $MESAJ
-echo -e $BOSLUK
-echo "⌛ N Σ O N YÜKLƏYİRƏM⌛"
-echo "⌛ I DOWNLOAD THE SIRI ⌛"
-echo -e $BOSLUK
-git clone -b siri_installer https://github.com/TheOksigen/neon_userbot
+cd Installer
 clear
-echo -e $N
-echo -e $BOSLUK
+echo "Bəzi Alt Yapi Dosyaları Yüklənir"
 echo -e $MESAJ
-echo -e $BOSLUK
-echo "⌛ INSTALLING REQUIREMENTS..."
-echo -e $BOSLUK
-cd neoninstaller
 pip install wheel
 pip install -r requirements.txt
 python -m neon_installer
